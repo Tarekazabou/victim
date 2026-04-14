366e5af4ca2603e4fc366d78a62f678e049f210f
Tarekazabou <tarek.azabou@supcom.tn>
2026-04-15 00:28:41 +0100

    fix: Update KubeSentinel build process and enhance monitoring script

--- run_monitoring.bash
+++ run_monitoring.bash
@@ -1,3 +1,9 @@
+#open port 8080 for kubesentinel monitoring
 kubectl logs -n falco -l app.kubernetes.io/name=falco -f --all-containers \
   | grep --line-buffered '^{' \
   | ./bin/kubesentinel monitor-stdin
+
+fuser -k 8080/tcp
+fuser -k 5000/tcp
+#kubectl port-forward svc/kubesentinel-ai -n kubesentinel 8080:5000 &
+kubectl port-forward svc/kubesentinel-ai -n kubesentinel 5000:5000 &
